32fd6a17fdf81adba4d1ed00ee6c9180819209e8
Andreu Masferrer <andreumasferrer@outlook.com>
2015-07-24 12:50:56 +0200

    Added devise gem & configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TO DO - AMP: Devise requirements
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  
 end
