4b564357181e308cd59135b219a0bd5f57e7c5c4
Andreu Masferrer <andreumasferrer@outlook.com>
2015-07-28 14:11:09 +0200

    Redirect to plans_path if logined + page flow improvements

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,9 +1,11 @@
 class PlansController < ApplicationController
 
   def index
+    @plans = Plan.all
   end
 
   def show
+    @plan = Plan.find(params[:id]) || (render "layouts/404")
   end
 
   def new
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,5 @@
 class SiteController < ApplicationController
   def home
+    redirect_to plans_path if user_signed_in? 
   end
 end
