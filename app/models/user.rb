7fe568e429d7e8fa3d4556f471d808c9709f3246
Andreu Masferrer <andreumasferrer@outlook.com>
2015-08-03 20:06:21 +0200

    Added user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   has_many :plan_subscriptions, dependent: :destroy
   has_many :subscribed_plans, source: :plan, through: :plan_subscriptions
 
+
+  def show_name
+    return first_name+" "+last_name unless first_name.nil?
+    email
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729205747) do
+ActiveRecord::Schema.define(version: 20150803162443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(version: 20150729205747) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
