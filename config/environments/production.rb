26ab3f3ac1c350400d6b3fcc268aded1e8f6fc94
Andreu Masferrer <andreumasferrer@outlook.com>
2015-08-05 10:21:47 +0200

    Fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # TO DO - AMP: Devise requirements
-  config.action_mailer.default_url_options = { host: 'http://plansapp.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://xlplans.herokuapp.com'}
 
   # AMP: Configure ActionMailer
   config.action_mailer.delivery_method = :smtp
